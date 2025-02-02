c24b99bb13463e5a95585330467ad270a897268b
Zhandos-prog <sutemgenov.zhandos@gmail.com>
2025-02-02 15:07:10 +0500

    Add basic state management for Circuit Breaker

--- breakr.go
+++ breakr.go
@@ -1,15 +1,47 @@
 package breakr
 
+import (
+	"github.com/genov8/breakr/internal"
+	"sync"
+)
+
 type Breaker struct {
+	mu     sync.Mutex
+	state  internal.State
 	config Config
 }
 
 func New(config Config) *Breaker {
 	return &Breaker{
+		state:  internal.Closed,
 		config: config,
 	}
 }
 
+func (b *Breaker) State() internal.State {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	return b.state
+}
+
 func (b *Breaker) Execute(fn func() (interface{}, error)) (interface{}, error) {
-	return nil, nil
+	b.mu.Lock()
+	if b.state == internal.Open {
+		b.mu.Unlock()
+		return nil, ErrCircuitOpen
+	}
+	b.mu.Unlock()
+
+	result, err := fn()
+
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	if err != nil {
+		b.state = internal.Open
+		return nil, err
+	}
+
+	b.state = internal.Closed
+	return result, nil
 }
---
+++ internal/state.go
@@ -0,0 +1,22 @@
+package internal
+
+type State int
+
+const (
+	Closed State = iota
+	Open
+	HalfOpen
+)
+
+func (s State) String() string {
+	switch s {
+	case Closed:
+		return "Closed"
+	case Open:
+		return "Open"
+	case HalfOpen:
+		return "Half-Open"
+	default:
+		return "Unknown"
+	}
+}
