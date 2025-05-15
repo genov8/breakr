4a1ba4ad2852f0ef6ec07280d9b063ab8d8f087e
genov8 <sutemgenov.zhandos@gmail.com>
2025-05-15 19:16:44 +0500

    add validate config

--- config/config.go
+++ config/config.go
@@ -1,6 +1,9 @@
 package config
 
-import "time"
+import (
+	"errors"
+	"time"
+)
 
 type Config struct {
 	FailureThreshold int
@@ -8,4 +11,17 @@ type Config struct {
 	ExecutionTimeout time.Duration
 	WindowSize       time.Duration
 	FailureCodes     []int
+}
+
+func (c Config) Validate() error {
+	if c.FailureThreshold <= 0 {
+		return errors.New("FailureThreshold must be > 0")
+	}
+	if c.ResetTimeout <= 0 {
+		return errors.New("ResetTimeout must be > 0")
+	}
+	if c.ExecutionTimeout <= 0 {
+		return errors.New("ExecutionTimeout must be > 0")
+	}
+	return nil
 }
